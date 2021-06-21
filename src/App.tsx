2d651b97b97372964a3a2c285964859d1252aa3d
Carlos Wimmer <coiote777@gmail.com>
2021-06-21 00:31:57 -0300

    Components, Props and State

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "./components/Button";
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Button>Clique aqui</Button>
+      <Button />
+      <Button />
+      <Button />
+    </>
   );
 }
 
