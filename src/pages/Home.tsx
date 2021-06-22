be0c64b6e96ddc99e51fb5f8adb8841b9e1ca1c1
Carlos Wimmer <coiote777@gmail.com>
2021-06-22 17:50:35 -0300

    Create authentication with Google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,14 +4,19 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import { auth, firebase } from '../services/firebase'
 import { Button } from '../components/Button'
 import '../styles/auth.scss'
 
 export function Home() {
   const history = useHistory()
 
-  function navigateToNewRoom() {
-    history.push('/rooms/new')
+  async function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+    const result = await auth.signInWithPopup(provider)
+    console.log(result)
+    // history.push('/rooms/new')
   }
 
   return (
@@ -27,7 +32,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Logo do Letmeask" />
-          <button onClick={navigateToNewRoom} className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
