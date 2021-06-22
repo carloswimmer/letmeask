4a49d33d812d1fa7e65a5587722ce5c29783c94f
Carlos Wimmer <coiote777@gmail.com>
2021-06-22 17:08:12 -0300

    Implement router and links

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
 
 import { Button } from '../components/Button'
 import '../styles/auth.scss'
@@ -25,8 +26,8 @@ export function NewRoom() {
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
-            Quer entrar em uma sala existente?
-            <a href="#">clique aqui</a>
+            Quer entrar em uma sala existente?&nbsp;
+            <Link to="/">clique aqui</Link>
           </p>
         </div>
       </main>
