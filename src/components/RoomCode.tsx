7867fb6955678e16cb75cfdd3a04c4d9ba7c7468
Carlos Wimmer <coiote777@gmail.com>
2021-06-23 20:01:10 -0300

    Create room page

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import copyImg from '../assets/images/copy.svg'
+
+import '../styles/room-code.scss'
+
+type RoomCodeProps = {
+  code: string
+}
+
+export const RoomCode = ({ code }: RoomCodeProps) => {
+  function copyToClipboard() {
+    navigator.clipboard.writeText(code)
+  }
+
+  return (
+    <button className="room-code" onClick={copyToClipboard}>
+      <div>
+        <img src={copyImg} alt="Copy room code" />
+      </div>
+      <span>Sala #{code.slice(1)}</span>
+    </button>
+  )
+}
