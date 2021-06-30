2961dc9cf6ba35253f606be509de2c6d2a07bdc8
Carlos Wimmer <coiote777@gmail.com>
2021-06-29 23:10:25 -0300

    Implement confirmation modal  in AdminRoom page

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-// import toast from 'react-hot-toast'
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 import { useHistory, useParams } from 'react-router-dom'
 
 import deleteImg from '../assets/images/delete.svg'
+import endDangerIcon from '../assets/images/end-danger.svg'
+import deleteDangerIcon from '../assets/images/delete-danger.svg'
 
 import { Button } from '../components/Button'
 import { Header } from '../components/Header'
@@ -10,6 +12,7 @@ import { RoomCode } from '../components/RoomCode'
 import { Question } from '../components/Question'
 import { CheckIcon } from '../components/CheckIcon'
 import { AnswerIcon } from '../components/AnswerIcon'
+import { ConfirmationModal } from '../components/ConfirmationModal'
 // import { useAuth, User } from '../hooks/auth'
 import { useRoom } from '../hooks/room'
 
@@ -22,25 +25,42 @@ type RoomParams = {
 
 export const AdminRoom = () => {
   // const { user } = useAuth()
+  const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false)
+  const [endModalIsOpen, setEndModalIsOpen] = useState(false)
+  const [editedId, setEditedId] = useState('')
   const params = useParams<RoomParams>()
   const roomId = params.id
   const { questions, title } = useRoom(roomId)
   const history = useHistory()
 
-  async function handleEndRoom() {
-    if (window.confirm('Are you sure you want to close this room?')) {
-      await database.ref(`rooms/${roomId}`).update({
-        endedAt: new Date(),
-      })
-    }
+  function cancelAction() {
+    setDeleteModalIsOpen(false)
+    setEndModalIsOpen(false)
+    setEditedId('')
+  }
 
+  function handleEndRoom() {
+    setEndModalIsOpen(true)
+  }
+
+  async function confirmEndRoom() {
+    await database.ref(`rooms/${roomId}`).update({
+      endedAt: new Date(),
+    })
+    setEndModalIsOpen(false)
     history.push('/')
   }
 
-  async function handleDeleteQuestion(questionId: string) {
-    if (window.confirm('Are you sure you want to delete?')) {
-      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
-    }
+  function handleDeleteQuestion(questionId: string) {
+    setEditedId(questionId)
+    setDeleteModalIsOpen(true)
+  }
+
+  async function confirmDeleteQuestion() {
+    await database.ref(`rooms/${roomId}/questions/${editedId}`).remove()
+    setDeleteModalIsOpen(false)
+    setEditedId('')
+    toast.success('Pergunta excluída com sucesso.')
   }
 
   async function handleCheckQuestionAsAnswered(
@@ -103,7 +123,11 @@ export const AdminRoom = () => {
                     onClick={() => handleHighlighQuestion(id, isHighlighted)}
                   >
                     <AnswerIcon
-                      color={isHighlighted ? 'purple-icon' : 'gray-icon'}
+                      color={
+                        isHighlighted && !isAnswered
+                          ? 'purple-icon'
+                          : 'gray-icon'
+                      }
                       alt="Dar destaque à pergunta"
                     />
                   </button>
@@ -118,6 +142,28 @@ export const AdminRoom = () => {
             )}
         </div>
       </main>
+
+      <ConfirmationModal
+        open={deleteModalIsOpen}
+        icon={deleteDangerIcon}
+        alt="Trash can icon"
+        title="Excluir pergunta"
+        text="Tem certeza que você deseja excluir essa pergunta?"
+        cancelAction={cancelAction}
+        confirmAction={confirmDeleteQuestion}
+        confirmTextButton="Sim, excluir"
+      />
+
+      <ConfirmationModal
+        open={endModalIsOpen}
+        icon={endDangerIcon}
+        alt="Remove icon"
+        title="Encerrar sala"
+        text="Tem certeza que você deseja encerrar esta sala?"
+        cancelAction={cancelAction}
+        confirmAction={confirmEndRoom}
+        confirmTextButton="Sim, encerrar"
+      />
     </div>
   )
 }
