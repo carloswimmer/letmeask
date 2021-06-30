85f78ddfa2d4ee60a58c0e8de8a720a0478b94b0
Carlos Wimmer <coiote777@gmail.com>
2021-06-30 00:13:30 -0300

    Implement rounded button in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,38 @@
 import React, { ReactNode } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+
+import { auth } from '../services/firebase'
 
 import logoImg from '../assets/images/logo.svg'
+import powerIcon from '../assets/images/power.svg'
 
 import '../styles/header.scss'
+import { Button } from './Button'
 
 type HeaderProps = {
   children: ReactNode
 }
 
 export const Header = ({ children }: HeaderProps) => {
+  const history = useHistory()
+
+  async function signOut() {
+    await auth.signOut()
+    history.push('/')
+  }
+
   return (
     <header>
       <div className="content">
         <Link to="/">
           <img src={logoImg} alt="Logo Letmeask" />
         </Link>
-        <div>{children}</div>
+        <div>
+          {children}
+          <Button isOutlined isRounded onClick={signOut}>
+            <img src={powerIcon} alt="Fazer logout" />
+          </Button>
+        </div>
       </div>
     </header>
   )
